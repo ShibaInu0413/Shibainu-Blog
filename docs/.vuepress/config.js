122cb0007ef028f06e29da93b3ccce78f1d5f176
ShibaInu0413 <neo123440@gmail.com>
2021-08-04 19:39:49 +0800

    2021-08-04

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'ShibaInu\'s Blog',
+    description: '記錄各種事情的vuepress-blog',
+    dest: './blog',
+    head: [
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ],
+    theme: 'reco',
+    themeConfig: {
+        type: 'blog',
+        logo: '/logo.png',
+        authorAvatar: '/logo.png'
+    }
+}
---
+++ docs/daily/first.md
@@ -0,0 +1,10 @@
+---
+title: First Day
+date: 2021-08-04
+tags:
+ - daily
+---
+
+沒啥可說的  就...
+
+**第一天**
