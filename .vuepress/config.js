5db6033cee6c070d825a939bc3659b5f5ba577f3
ShibaInu0413 <neo123440@gmail.com>
2021-08-05 17:44:30 +0800

    add icon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,12 +2,17 @@ module.exports = {
     title: 'ShibaInu\'s Blog',
     description: '記錄各種事情的vuepress-blog',
     head: [
-        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+        ['link', { rel: "shortcut icon", href: "/icon/favicon.ico"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/icon/favicon-16x16.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/icon/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/icon/favicon-96x96.png"}],
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/icon/favicon-180x180.png"}]
     ],
     theme: 'reco',
     themeConfig: {
         type: 'blog',
-        logo: '/logo.png',
-        authorAvatar: '/logo.png'
+        logo: '/icon/logo.png',
+        authorAvatar: '/icon/logo.png'
     }
 }
