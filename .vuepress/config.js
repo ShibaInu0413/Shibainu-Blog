b41da132ad2fa6923eeb5145edcfc5fc4807bcf3
ShibaInu0413 <neo123440@gmail.com>
2021-08-07 00:07:18 +0800

    /icon -> /assets/icon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,17 +3,17 @@ module.exports = {
     description: '記錄各種事情的vuepress-blog',
     head: [
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ['link', { rel: "shortcut icon", href: "/icon/favicon.ico"}],
-        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/icon/favicon-16x16.png"}],
-        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/icon/favicon-32x32.png"}],
-        ['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/icon/favicon-96x96.png"}],
-        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/icon/favicon-180x180.png"}]
+        ['link', { rel: "shortcut icon", href: "/assets/icon/favicon.ico"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/icon/favicon-16x16.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/icon/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/assets/icon/favicon-96x96.png"}],
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/icon/favicon-180x180.png"}]
     ],
     theme: 'reco',
     themeConfig: {
         type: 'blog',
-        logo: '/icon/logo.png',
-        authorAvatar: '/icon/logo.png',
+        logo: '/assets/icon/logo.png',
+        authorAvatar: '/assets/icon/logo.png',
         nav: [
             { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }
         ],
