9264189432d9fa532189810e5840bc8e4cafa6c2
ShibaInu0413 <neo123440@gmail.com>
2021-08-10 23:19:08 +0800

    add google adsense

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,14 +3,16 @@ module.exports = {
     description: '\n',
     head: [
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ['link', { rel: "shortcut icon", href: "/assets/icon/favicon.ico" }],
-        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/icon/favicon-16x16.png" }],
-        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/icon/favicon-32x32.png" }],
-        ['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/assets/icon/favicon-96x96.png" }],
-        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/icon/favicon-180x180.png" }],
+        ['link', { rel: 'shortcut icon', href: '/assets/icon/favicon.ico' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/icon/favicon-16x16.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/icon/favicon-32x32.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/assets/icon/favicon-96x96.png' }],
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/icon/favicon-180x180.png' }],
 
-        ['link', { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css" }],
-        ['script', { src: "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js" }],
+        ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css' }],
+        ['script', { src: 'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js' }],
+
+        ['script', { 'data-ad-client':'ca-pub-2021556311985834', async: true, src:'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' }]
     ],
     theme: 'reco',
     themeConfig: {
