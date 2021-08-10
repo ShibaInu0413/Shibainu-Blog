35b6fa89a07d1028f3e03d49ef0ad883e1029540
ShibaInu0413 <neo123440@gmail.com>
2021-08-10 21:35:54 +0800

    add plugins

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,13 +1,16 @@
 module.exports = {
     title: 'ShibaInu\'s Blog',
-    description: '記錄各種事情的vuepress-blog',
+    description: '\n',
     head: [
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ['link', { rel: "shortcut icon", href: "/assets/icon/favicon.ico"}],
-        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/icon/favicon-16x16.png"}],
-        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/icon/favicon-32x32.png"}],
-        ['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/assets/icon/favicon-96x96.png"}],
-        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/icon/favicon-180x180.png"}]
+        ['link', { rel: "shortcut icon", href: "/assets/icon/favicon.ico" }],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/icon/favicon-16x16.png" }],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/icon/favicon-32x32.png" }],
+        ['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/assets/icon/favicon-96x96.png" }],
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/icon/favicon-180x180.png" }],
+
+        ['link', { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css" }],
+        ['script', { src: "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js" }],
     ],
     theme: 'reco',
     themeConfig: {
@@ -15,21 +18,25 @@ module.exports = {
         logo: '/assets/icon/logo.png',
         authorAvatar: '/assets/icon/logo.png',
         nav: [
-            { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }
+            { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+            { text: 'RSS', link: 'https://blog.dshibainu.com/rss.xml', icon: 'reco-bokeyuan' }
         ],
         noFoundPageByTencent: false,
-        friendLink: [
-            {
-                title: 'RSS',
-                desc: 'RSS link of this site.',
-                logo: '/assets/icon/rss.png',
-                link: '/rss.xml'
-            }
-        ]
+        friendLink: []
     },
     plugins: {
-        "@vuepress-reco/vuepress-plugin-rss": {
+        '@vuepress-reco/vuepress-plugin-rss': {
             site_url: 'https://blog.dshibainu.com'
+        },
+        'dynamic-title': {
+            showText: '(ฅ>ω<*ฅ)欢迎回来！',
+            hideText: '( ๑ˊ•̥▵•)੭₎₎不要走呀！',
+            recoverTime: 2000,
+        },
+        'cursor-effects': {
+            size: 2, // size of the particle, default: 2
+            shape: ['star'], // shape of the particle, default: 'star'
+            zIndex: 999999999, // z-index property of the canvas, default: 999999999
         }
     }
 }
